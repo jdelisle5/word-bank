454c542b1aac7c8811ec63cf96dd719ad1fc3567
jdelisle5 <jdelisle5@gmail.com>
2019-03-10 08:29:48 -0400

    Second Changes: added some meat

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var textbox = prompt("Please enter your text", "Harry Potter");
+
+if (textbox != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
